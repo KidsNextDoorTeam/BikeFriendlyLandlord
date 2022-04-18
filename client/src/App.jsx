6defe9b73584d0b79a35540b21c70881dfdbfb88
Stephan Halarewicz <shalarewicz@gmail.com>
2022-04-18 19:06:56 -0400

    Add Alert banner mechanishm. Flash login alert if user tries to access protected route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,32 +12,38 @@ import UserProfile from './pages/UserProfile';
 import ProtectedRoute from './components/ProtectedRoute';
 import useAuth from './hooks/useAuth';
 import UserContext from './hooks/userContext';
-
+import Alerts from './components/Alerts';
+import AlertContext from './hooks/alertContext';
+import useAlert from './hooks/useAlert';
 // Global styling
 import './index.css';
 
 export function App() {
   const { isLoading, user, setUser } = useAuth();
+  const { alert, setAlert, alertSeverity, setAlertSeverity } = useAlert();
 
   return (
     <UserContext.Provider value={{ user, setUser, isLoading }}>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/map" element={<MapSearch />} />
-        <Route path="/landlord/:landlord_id" element={<Profile />} />
-        <Route
-          path="/review/:landlord_id"
-          element={<ProtectedRoute><ReviewPage /></ProtectedRoute>}
-        />
-        <Route
-          path="/profile/:username"
-          element={<ProtectedRoute><UserProfile /></ProtectedRoute>}
-        />
-        <Route path="*" element={<p>404 - nothing here</p>} />
-      </Routes>
-      <Footer />
+      <AlertContext.Provider value={{ alert, setAlert, alertSeverity, setAlertSeverity }}>
+        <Navbar />
+        <Alerts />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/map" element={<MapSearch />} />
+          <Route path="/landlord/:landlord_id" element={<Profile />} />
+          <Route
+            path="/review/:landlord_id"
+            element={<ProtectedRoute><ReviewPage /></ProtectedRoute>}
+          />
+          <Route
+            path="/profile/:username"
+            element={<ProtectedRoute><UserProfile /></ProtectedRoute>}
+          />
+          <Route path="*" element={<p>404 - nothing here</p>} />
+        </Routes>
+        <Footer />
+      </AlertContext.Provider>
     </UserContext.Provider>
   );
 }
--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,12 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import UserContext from '../hooks/userContext';
+import AlertContext from '../hooks/alertContext';
 
 export default function ProtectedRoute({ children }) {
   const { user, isLoading } = useContext(UserContext);
+  const { setAlert, setAlertSeverity } = useContext(AlertContext);
 
   const location = useLocation();
 
+  useEffect(() => {
+    if (!isLoading && !user) {
+      // this needs to occur in useEffect to prevent App from 
+      // re-rendering while this component is re-rendering
+      setAlert('Please login to access this page');
+      setAlertSeverity('error');
+    }
+  });
   // TODO: Add a Loading component
   if (isLoading) return <div>Loading...</div>;
 
