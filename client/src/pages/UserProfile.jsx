84f34bd35008ed90290ec803df443c4f5aa5ec33
MariahTali <talicuran.mariah@gmail.com>
2022-04-21 15:05:14 -0700

    add review opens to select landlord drop down but no landlords populated yet. Still need to set onclick for create review button and route to review page

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,23 +1,32 @@
-import React, { useState, useEffect, useRef, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect, useRef, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Avatar from '@mui/material/Avatar';
-import { Button, Grid, TextField, Input } from '@material-ui/core';
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Avatar from "@mui/material/Avatar";
+import { Button, Grid, TextField, Input } from "@material-ui/core";
 
-import { Review } from '../components/Review';
-import { stringAvatar } from '../common/styling.js';
-import { useAuth } from '../hooks/authContext';
+import { Review } from "../components/Review";
+import { stringAvatar } from "../common/styling.js";
+import { useAuth } from "../hooks/authContext";
+
+// for landlord selecting
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 export default function UserProfile() {
-  const {user, user : {first_name}} = useAuth();
+  const {
+    user,
+    user: { first_name },
+  } = useAuth();
 
   const [reviews, setReviews] = useState([]);
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
   const [firstname, setFirstName] = useState(first_name);
 
   const [currentTab, setCurrentTab] = useState(0);
@@ -28,34 +37,35 @@ export default function UserProfile() {
   const mounted = useRef(true);
   const getReviews = async () => {
     if (!user._id) {
-      navigate('/'); // user needs to sign in
+      navigate("/"); // user needs to sign in
       return;
     }
     try {
       const { status, data } = await axios.get(`/user/${user._id}/reviews`);
-      if (status >= 200 && status < 300 && mounted.current) setReviews(data.reviews);
+      if (status >= 200 && status < 300 && mounted.current)
+        setReviews(data.reviews);
     } catch (err) {
       if (err?.repsonse?.status === 401) {
-        navigate('/');
+        navigate("/");
       } else {
-        console.error('Error fetching users reviews -->', err);
+        console.error("Error fetching users reviews -->", err);
       }
     }
   };
 
-   // state for addReview onclick leading to landlord selection
-   const [addReview, setAddReview] = useState(false);
+  // state for addReview onclick leading to landlord selection
+  const [addReview, setAddReview] = useState(false);
 
-   //state for landlord, set landlord state
-   const [landlord, setLandlord] = React.useState("");
-   //for changing landlord state
-   const handleChange = (event) => {
-     setLandlord(event.target.value);
-   };
+  //state for landlord, set landlord state
+  const [landlord, setLandlord] = React.useState("");
+  //for changing landlord state
+  const handleChange = (event) => {
+    setLandlord(event.target.value);
+  };
 
   useEffect(() => {
     getReviews();
-    return () => () => mounted.current = false;
+    return () => () => (mounted.current = false);
   }, [user]);
 
   const onReviewDelete = () => {
@@ -78,18 +88,18 @@ export default function UserProfile() {
 
   return (
     <div
-      id='userProfile'
+      id="userProfile"
       sx={{
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundAttachment: 'fixed',
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
       }}
     >
-      <Typography variant='h6' component='div' style={{ fontFamily: 'Nunito' }}>
+      <Typography variant="h6" component="div" style={{ fontFamily: "Nunito" }}>
         <Grid container spacing={1}>
-          <Grid item xs={3} >
-            <div style={{ display: 'flex' }}>
-              {user.profile_pic ?
+          <Grid item xs={3}>
+            <div style={{ display: "flex" }}>
+              {user.profile_pic ? (
                 <Avatar
                   alt="User picture"
                   src={`/images/${user.profile_pic}`}
@@ -99,57 +109,66 @@ export default function UserProfile() {
                     minWidth: 20,
                     minHeight: 20,
                     my: 2.5,
-                  }} />
-                : <Avatar
+                  }}
+                />
+              ) : (
+                <Avatar
                   alt="User picture"
                   {...stringAvatar(`${user.first_name} ${user.last_name}`)}
-                />}
+                />
+              )}
             </div>
-            {updateMode ?
+            {updateMode ? (
               <Box>
-                <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }} >
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    gap: "10px",
+                  }}
+                >
                   {/* TODO: Hanlde form updates and submission */}
                   <TextField
-                    label='First Name'
-                    variant='outlined'
+                    label="First Name"
+                    variant="outlined"
                     onChange={(e) => setFirstName(e.target.value)}
                     size="small"
                   />
                   <TextField
-                    label='Last Name'
-                    variant='outlined'
+                    label="Last Name"
+                    variant="outlined"
                     // onChange={(e) => setUsername(e.target.value)}
                     size="small"
                   />
                   <TextField
-                    label='Username'
-                    variant='outlined'
+                    label="Username"
+                    variant="outlined"
                     onChange={(e) => setUsername(e.target.value)}
                     size="small"
                   />
                   <TextField
-                    label='Bio'
-                    variant='outlined'
+                    label="Bio"
+                    variant="outlined"
                     multiline
                     rows={4}
-                  // onChange={(e) => setUsername(e.target.value)}
+                    // onChange={(e) => setUsername(e.target.value)}
                   />
-                  <Button variant='contained' component='label'>
-                    {' '}
+                  <Button variant="contained" component="label">
+                    {" "}
                     Upload Picture
-                    <input type='file' hidden />
+                    <input type="file" hidden />
                   </Button>
                 </div>
                 <button
                   style={{
-                    padding: '7px 15px',
-                    borderRadius: '10px',
-                    border: '1px solid tomato',
-                    color: 'tomato',
-                    backgroundColor: 'transparent',
-                    marginRight: '10px',
-                    cursor: 'pointer',
-                    marginTop: '10px',
+                    padding: "7px 15px",
+                    borderRadius: "10px",
+                    border: "1px solid tomato",
+                    color: "tomato",
+                    backgroundColor: "transparent",
+                    marginRight: "10px",
+                    cursor: "pointer",
+                    marginTop: "10px",
                   }}
                   onClick={() => {
                     setUpdateMode(false);
@@ -157,57 +176,6 @@ export default function UserProfile() {
                 >
                   Cancel
                 </button>
-                <button
-                  style={{
-                    padding: '7px 15px',
-                    borderRadius: '10px',
-                    border: 'none',
-                    color: 'white',
-                    backgroundColor: 'tomato',
-                    marginRight: '10px',
-                    cursor: 'pointer',
-                  }}
-                  onClick={userProfileChange}
-                >
-                  Save
-                </button>
-              </Box > :
-              <Box>
-                <div style={{ marginBottom: '10px', color: '#333' }}>
-                  <h2> {user.first_name} {user.last_name}</h2>
-                  <span> {user.username} </span>
-                </div>
-                <Button
-                  variant="contained"
-                  style={{
-                    width: '80%',
-                    backgroundColor: 'tomato',
-                    color: 'white'
-                  }}
-                  onClick={() => { setUpdateMode(true); }}
-                >
-                  Edit Profile
-                </Button>
-              </Box>
-            }
-            {addReview ? 
-              <Box sx={{ minWidth: 120 }}>
-                <FormControl fullWidth>
-                  <InputLabel id="demo-simple-select-label">
-                    Select Landlord
-                  </InputLabel>
-                  <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={landlord}
-                    label="landLords"
-                    onChange={handleChange}
-                  >
-                    <MenuItem value={10}>Ten</MenuItem>
-                   
-                  </Select>
-                </FormControl>
-
                 <button
                   style={{
                     padding: "7px 15px",
@@ -218,40 +186,45 @@ export default function UserProfile() {
                     marginRight: "10px",
                     cursor: "pointer",
                   }}
-                  //  onClick={}
+                  onClick={userProfileChange}
                 >
-                  Search
+                  Save
                 </button>
               </Box>
-             : 
+            ) : (
               <Box>
-                {/* adding review button that opens to select which landlord to add review */}
-
+                <div style={{ marginBottom: "10px", color: "#333" }}>
+                  <h2>
+                    {" "}
+                    {user.first_name} {user.last_name}
+                  </h2>
+                  <span> {user.username} </span>
+                </div>
                 <Button
                   variant="contained"
                   style={{
                     width: "80%",
                     backgroundColor: "tomato",
                     color: "white",
-                    marginTop: 12,
                   }}
                   onClick={() => {
-                    setAddReview(true);
+                    setUpdateMode(true);
                   }}
                 >
-                  Add Review
+                  Edit Profile
                 </Button>
               </Box>
-            }
-          </Grid >
-          <Grid item xs={9} >
+            )}
+            
+          </Grid>
+          <Grid item xs={9}>
             <Tabs
               textColor="inherit"
               variant="fullWidth"
               TabIndicatorProps={{
                 style: {
-                  backgroundColor: '#df4f35ea'
-                }
+                  backgroundColor: "#df4f35ea",
+                },
               }}
               value={currentTab}
               onChange={handleTabChange}
@@ -260,63 +233,122 @@ export default function UserProfile() {
               <Tab label="Reviews" />
               <Tab label="Saved Landlords" />
             </Tabs>
-            {currentTab === 0 &&
+            {currentTab === 0 && (
               <div id="userDetails">
-                {user.description
-                  ? <span
-                    style={{ marginTop: '2em' }}
-                  >
-                    {user.description}
-                  </span>
-                  : <span
+                {user.description ? (
+                  <span style={{ marginTop: "2em" }}>{user.description}</span>
+                ) : (
+                  <span
                     style={{
-                      textAlign: 'center',
-                      marginTop: '2em'
+                      textAlign: "center",
+                      marginTop: "2em",
                     }}
                   >
                     Welcome to Bike Friendly Landlord.<br></br>
                     Edit your profile, add a bio and explore!
-                  </span>}
-              </div>}
-            {currentTab === 1 && <div>
-              {reviews.length === 0 &&
-                <h3
-                  style={{
-                    textAlign: 'center',
-                    marginTop: '2em'
-                  }}
-                >
-                  You have no reviews yet
-                </h3>}
-              {reviews.map((review, index) => {
-                return <Review
-                  username={user.username}
-                  _id={review._id}
-                  title={review.title}
-                  overall_rating={review.overall_rating}
-                  respect_rating={review.respect_rating}
-                  responsiveness_rating={review.responsiveness_rating}
-                  bike_rating={review.bike_rating}
-                  pet_friendly_rating={review.pet_friendly}
-                  description={review.description}
-                  key={index}
-                  onSave={onReviewSave}
-                  onDelete={onReviewDelete}
-                />;
-              })}
-            </div>}
-            {currentTab === 2 &&
+                  </span>
+                )}
+              </div>
+            )}
+            {currentTab === 1 && (
+              <div>
+                {addReview ? (
+                  <Box sx={{ minWidth: 120 }}>
+                    <FormControl fullWidth>
+                      <InputLabel id="demo-simple-select-label">
+                        Select Landlord
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={landlord}
+                        label="landLords"
+                        onChange={handleChange}
+                      >
+                        <MenuItem value={10}>Ten</MenuItem>
+                      </Select>
+                    </FormControl>
+
+                    <button
+                      style={{
+                        padding: "7px 15px",
+                        borderRadius: "10px",
+                        border: "none",
+                        color: "white",
+                        backgroundColor: "tomato",
+                        marginRight: "10px",
+                        cursor: "pointer",
+                        marginBottom: 10,
+                      }}
+                      //  onClick={} need to select landlordid and send to review page router
+                    >
+                      Create Review
+                    </button>
+                  </Box>
+                ) : (
+                  <Box>
+                    {/* adding review button that opens to select which landlord to add review */}
+
+                    <Button
+                      variant="contained"
+                      style={{
+                        width: "20%",
+                        backgroundColor: "tomato",
+                        color: "white",
+                        marginTop: 12,
+                        marginBottom: 10,
+                      }}
+                      onClick={() => {
+                        setAddReview(true);
+                      }}
+                    >
+                      Add Review
+                    </Button>
+                  </Box>
+                )}
+                {reviews.length === 0 && (
+                  <h3
+                    style={{
+                      textAlign: "center",
+                      marginTop: "2em",
+                    }}
+                  >
+                    You have no reviews yet
+                  </h3>
+                )}
+                {reviews.map((review, index) => {
+                  return (
+                    <Review
+                      username={user.username}
+                      _id={review._id}
+                      title={review.title}
+                      overall_rating={review.overall_rating}
+                      respect_rating={review.respect_rating}
+                      responsiveness_rating={review.responsiveness_rating}
+                      bike_rating={review.bike_rating}
+                      pet_friendly_rating={review.pet_friendly}
+                      description={review.description}
+                      key={index}
+                      onSave={onReviewSave}
+                      onDelete={onReviewDelete}
+                    />
+                  );
+                })}
+              </div>
+            )}
+            {currentTab === 2 && (
               <div
                 style={{
-                  marginTop: '50px',
-                  textAlign: 'center'
+                  marginTop: "50px",
+                  textAlign: "center",
                 }}
               >
                 <h3> You don&apos;t have any saved landlords yet</h3>
-              </div>}
+              </div>
+            )}
           </Grid>
-        </Grid >
-      </Typography >
-    </div >
+        </Grid>
+      </Typography>
+    </div>
   );
 }
